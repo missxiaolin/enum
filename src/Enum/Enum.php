1377b3090a75b711dff4fe404e8bf0bdf3011a27
xiaolin <462441355@qq.com>
2017-11-24 09:47:37 +0800

    * add

---
+++ src/Enum/Enum.php
@@ -0,0 +1,91 @@
+<?php
+namespace xiaolin\Enum;
+// +----------------------------------------------------------------------
+// | EnumException.php [ WE CAN DO IT JUST THINK IT ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2016-2017 xiaolin All rights reserved.
+// +----------------------------------------------------------------------
+// | Author: limx <462441355@qq.com> <https://github.com/missxiaolin>
+// +----------------------------------------------------------------------
+
+use Phalcon\Text;
+use Phalcon\Annotations\Adapter\Memory as MemoryAdapter;
+use ReflectionClass;
+use xiaolin\Enum\Exception\EnumException;
+
+abstract class Enum
+{
+    public static $_instance;
+
+    public $_adapter = 'memory';
+
+    public $_expire = 3600;
+
+    public function __construct()
+    {
+
+    }
+
+    /**
+     * @return static
+     */
+    public static function getInstance()
+    {
+        if (isset(static::$_instance) && static::$_instance instanceof Enum) {
+            return static::$_instance;
+        }
+
+        return static::$_instance = new static();
+    }
+
+    /**
+     * @param $method
+     * @param $arguments
+     * @return mixed
+     */
+    public static function __callStatic($method, $arguments)
+    {
+        return static::getInstance()->$method(...$arguments);
+    }
+
+    /**
+     * @param $name
+     * @param $arguments
+     * @return string
+     * @throws EnumException
+     */
+    public function __call($name, $arguments)
+    {
+        if (!Text::startsWith($name, 'get')) {
+            throw new EnumException('The function is not defined!');
+        }
+        if (!isset($arguments) || count($arguments) === 0) {
+            throw new EnumException('The Code is required');
+        }
+
+        $code = $arguments[0];
+        $name = strtolower(substr($name, 3));
+
+        if (isset($this->$name)) {
+            return isset($this->$name[$code]) ? $this->$name[$code] : '';
+        }
+
+        // 获取注释
+        $adapter = new MemoryAdapter();
+        $reflection = $adapter->get(static::class);
+        $annotations = $reflection->getPropertiesAnnotations();
+
+        // 获取变量
+        $ref = new ReflectionClass(static::class);
+        $properties = $ref->getDefaultProperties();
+        foreach ($properties as $key => $val) {
+            if (Text::startsWith($key, 'ENUM_') && isset($annotations[$key])) {
+                // 获取对应注释
+                $ret = $annotations[$key]->get(Text::camelize($name));
+                $this->$name[$val] = $ret->getArgument(0);
+            }
+        }
+
+        return isset($this->$name[$code]) ? $this->$name[$code] : '';
+    }
+}
---
+++ tests/Str/BaseTest.php
@@ -0,0 +1,31 @@
+<?php
+namespace Tests\Str;
+// +----------------------------------------------------------------------
+// | BaseTest.php [ WE CAN DO IT JUST THINK IT ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2016-2017 xiaolin All rights reserved.
+// +----------------------------------------------------------------------
+// | Author: xiaolin <462441355@qq.com> <https://github.com/missxiaolin>
+// +----------------------------------------------------------------------
+
+
+use PHPUnit\Framework\TestCase;
+use Tests\App\ErrorCode;
+
+class BaseTest extends TestCase
+{
+    public function testAscii()
+    {
+        $this->assertEquals(700, ErrorCode::$ENUM_INVALID_TOKEN);
+    }
+
+    public function testMessage()
+    {
+        $this->assertEquals('非法的TOKEN', ErrorCode::getMessage(ErrorCode::$ENUM_INVALID_TOKEN));
+    }
+
+    public function testDesc()
+    {
+        $this->assertEquals('需要重新登录', ErrorCode::getDesc(ErrorCode::$ENUM_INVALID_TOKEN));
+    }
+}
